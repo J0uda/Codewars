cf290a58072d1fc06c937a7244c8f82b8eb4896a
Amine Jaoued <amine.jaoued123@gmail.com>
2023-12-07 16:55:56 +0100

    add some chalenges

--- 7-kyu/GA-DE-RY-PO-LU-KI cypher.js
+++ 7-kyu/GA-DE-RY-PO-LU-KI cypher.js
@@ -27,3 +27,41 @@ Example
  decode("GBCE")          // => ABCD
 GADERYPOLUKI collection
  */
+
+let enocding = {
+  a: 'g',
+  A: 'G',
+  D: 'E',
+  d: 'e',
+  R: 'Y',
+  r: 'y',
+  P: 'O',
+  p: 'o',
+  L: 'U',
+  l: 'u',
+  K: 'I',
+  k: 'i',
+  G: 'A',
+  g: 'a',
+  E: 'D',
+  e: 'd',
+  Y: 'R',
+  y: 'r',
+  O: 'P',
+  o: 'p',
+  U: 'L',
+  u: 'l',
+  I: 'K',
+  i: 'k',
+};
+
+function encode(str) {
+  return str
+    .split('')
+    .map((a) => enocding[a] || a)
+    .join('');
+}
+
+function decode(str) {
+  return encode(str);
+}
